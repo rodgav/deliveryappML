04ce948fb7312760cc702f364f17f3df1044d262
RSGM <bankenro.myhope@gmail.com>
2020-12-10 17:53:08 -0500

    fix scroll in prod detail.dart

--- lib/pages/detail.dart
+++ lib/pages/detail.dart
@@ -1,142 +1,108 @@
+import 'dart:typed_data';
+
 import 'package:deliveryapp1mauriciolopez/helpers/colors.dart';
-import 'package:deliveryapp1mauriciolopez/helpers/icons.dart';
+import 'package:deliveryapp1mauriciolopez/widgets/appBarDetail.dart';
 import 'package:deliveryapp1mauriciolopez/widgets/button.dart';
 import 'package:deliveryapp1mauriciolopez/widgets/itemIngre.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+import 'dart:ui' as ui;
 
-class DetailPage extends StatelessWidget {
+import 'package:flutter/services.dart';
+
+class DetailPage extends StatefulWidget {
+  @override
+  _DetailPageState createState() => _DetailPageState();
+}
+
+class _DetailPageState extends State<DetailPage> {
   List<ItemIngreWid> ingreds = [
     ItemIngreWid(asset: 'assets/images/arrachera.png', name: 'Arrachera'),
     ItemIngreWid(asset: 'assets/images/pan.png', name: 'Pan ajonjoli'),
     ItemIngreWid(asset: 'assets/images/lechuga.png', name: 'Lechuga'),
     ItemIngreWid(asset: 'assets/images/cebolla.png', name: 'Cebolla'),
   ];
+  ui.Image _image;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _loadImage();
+  }
 
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return Scaffold(
-      body: Stack(
-        children: [
-          Image.asset(
-            'assets/images/hambEspecial.png',
-            fit: BoxFit.fill,
-            width: size.width,
-            height: size.height * 0.6,
-          ),
-          Container(
-              width: size.width,
-              height: size.height,
-              decoration: BoxDecoration(),
-              child: CustomPaint(
-                painter: _Painter(),
-                child: Container(
-                  margin: EdgeInsets.symmetric(vertical: 0, horizontal: 20),
-                  child: Column(
-                      mainAxisAlignment: MainAxisAlignment.start,
+        backgroundColor: ColorsH.bg,
+        body: CustomScrollView(
+          physics: BouncingScrollPhysics(),
+          slivers: [
+            SliverPersistentHeader(
+              delegate: AppBarDetailWid(
+                  image: _image, expandedHeight: size.height * 0.5, size: size),
+              pinned: true,
+            ),
+            SliverList(
+              delegate: SliverChildListDelegate(
+                [
+                  Container(
+                    padding: EdgeInsets.symmetric(horizontal: 20),
+                    child: Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        SafeArea(
-                            child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            IconButton(
-                              icon: Icon(
-                                Icons.arrow_back_ios_sharp,
-                                color: ColorsH.white,
-                              ),
-                              onPressed: () {
-                                Navigator.of(context).pop();
-                              },
-                            ),
-                            Text(
-                              'Hamburguesa especial',
-                              style:
-                                  TextStyle(color: ColorsH.white, fontSize: 16),
-                            ),
-                            Container(
-                              width: 24,
-                              height: 0,
-                            )
-                          ],
-                        )),
                         Container(
-                          height: size.height * 0.4,
-                        ),
-                        Container(
-                          height: size.height * 0.35,
-                          child: SingleChildScrollView(
-                            physics: BouncingScrollPhysics(),
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Container(
-                                  margin: EdgeInsets.only(left: 7),
-                                  child: Text(
-                                    'Descipción',
-                                    style: TextStyle(
-                                        color: ColorsH.text, fontSize: 18),
-                                  ),
-                                ),
-                                SizedBox(
-                                  height: 10,
-                                ),
-                                Text(
-                                  'Lorem ipsum dolor sit amet, consetetur sadipscing '
-                                  'elitr, sed diam nonumy eirmod tempor invidunt ut'
-                                  ' labore et dolore magna aliquyam erat, sed diam '
-                                  'voluptua. At vero eos et accusam et justo duo dolores '
-                                  'et ea rebum. Stet clita kasd gubergren, no sea takimata '
-                                  'sanctus est Lorem ipsum dolor sit amet. Lorem ipsum'
-                                  ' dolor sit amet, consetetur',
-                                  style: TextStyle(
-                                      color: ColorsH.text, fontSize: 11),
-                                ),
-                                SizedBox(
-                                  height: 20,
-                                ),
-                                Container(
-                                    margin: EdgeInsets.only(left: 7),
-                                    child: Row(
-                                      mainAxisAlignment:
-                                          MainAxisAlignment.spaceBetween,
-                                      children: [
-                                        Text(
-                                          'Ingredientes',
-                                          style: TextStyle(
-                                              color: ColorsH.text,
-                                              fontSize: 18),
-                                        ),
-                                        Text(
-                                          '10 ingredientes',
-                                          style: TextStyle(
-                                              color: ColorsH.text4,
-                                              fontSize: 10),
-                                        ),
-                                      ],
-                                    )),
-                                SizedBox(
-                                  height: 10,
-                                ),
-                                Container(
-                                    height: 180,
-                                    child: ListView(
-                                      shrinkWrap: true,
-                                      scrollDirection: Axis.horizontal,
-                                      physics: BouncingScrollPhysics(),
-                                      children: ingreds,
-                                    )),
-                                SizedBox(
-                                  height: 10,
-                                ),
-                              ],
-                            ),
+                          margin: EdgeInsets.only(left: 7),
+                          child: Text(
+                            'Descipción',
+                            style: TextStyle(color: ColorsH.text, fontSize: 18),
                           ),
                         ),
-                        Divider(
-                          height: 5,
+                        SizedBox(
+                          height: 10,
                         ),
+                        Text(
+                          'Lorem ipsum dolor sit amet, consetetur sadipscing '
+                          'elitr, sed diam nonumy eirmod tempor invidunt ut'
+                          ' labore et dolore magna aliquyam erat, sed diam '
+                          'voluptua. At vero eos et accusam et justo duo dolores '
+                          'et ea rebum. Stet clita kasd gubergren, no sea takimata '
+                          'sanctus est Lorem ipsum dolor sit amet. Lorem ipsum'
+                          ' dolor sit amet, consetetur',
+                          style: TextStyle(color: ColorsH.text, fontSize: 11),
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        Container(
+                            margin: EdgeInsets.only(left: 7),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text(
+                                  'Ingredientes',
+                                  style: TextStyle(
+                                      color: ColorsH.text, fontSize: 18),
+                                ),
+                                Text(
+                                  '10 ingredientes',
+                                  style: TextStyle(
+                                      color: ColorsH.text4, fontSize: 10),
+                                ),
+                              ],
+                            )),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Container(
+                            height: 180,
+                            child: ListView(
+                              shrinkWrap: true,
+                              scrollDirection: Axis.horizontal,
+                              physics: BouncingScrollPhysics(),
+                              children: ingreds,
+                            )),
                         SizedBox(
                           height: 10,
                         ),
@@ -156,54 +122,22 @@ class DetailPage extends StatelessWidget {
                               ),
                             ],
                           ),
-                        )
-                      ]),
-                ),
-              )),
-          Positioned(
-            top: (size.height * 0.40) - 24,
-            right: size.width * 0.15,
-            child: Container(
-              width: 48,
-              height: 48,
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(100),
-                  color: ColorsH.rosa),
-              child: Icon(
-                IconsH.nofavorite,
-                color: ColorsH.white,
+                        ),
+                      ],
+                    ),
+                  )
+                ],
               ),
-            ),
-          )
-        ],
-      ),
-    );
-  }
-}
-
-class _Painter extends CustomPainter {
-  @override
-  void paint(Canvas canvas, Size size) {
-    // TODO: implement paint
-    final paint = Paint();
-    paint.color = ColorsH.bg;
-    paint.style = PaintingStyle.fill;
-    paint.strokeWidth = 5;
-    final path = Path();
-    path.moveTo(0, size.height);
-    path.lineTo(0, size.height * 0.5);
-    path.quadraticBezierTo(
-        0, size.height * 0.40, size.width * 0.3, size.height * 0.40);
-    path.lineTo(size.width * 0.80, size.height * 0.40);
-    path.quadraticBezierTo(
-        size.width, size.height * 0.4, size.width, size.height * 0.30);
-    path.lineTo(size.width, size.height);
-    path.lineTo(0, size.height);
-    canvas.drawPath(path, paint);
+            )
+          ],
+        ));
   }
 
-  @override
-  bool shouldRepaint(covariant CustomPainter oldDelegate) {
-    return true;
+  _loadImage() async {
+    ByteData bd = await rootBundle.load('assets/images/hambEspecial.png');
+    final Uint8List bytes = Uint8List.view(bd.buffer);
+    final ui.Codec codec = await ui.instantiateImageCodec(bytes);
+    final ui.Image image = (await codec.getNextFrame()).image;
+    setState(() => _image = image);
   }
 }
---
+++ lib/widgets/appBarDetail.dart
@@ -0,0 +1,119 @@
+import 'package:deliveryapp1mauriciolopez/helpers/colors.dart';
+import 'package:deliveryapp1mauriciolopez/helpers/icons.dart';
+import 'package:flutter/material.dart';
+import 'dart:ui' as ui;
+
+class AppBarDetailWid extends SliverPersistentHeaderDelegate {
+  final double expandedHeight;
+  ui.Image image;
+  final Size size;
+
+  AppBarDetailWid({this.expandedHeight, this.image, this.size});
+
+  @override
+  Widget build(
+      BuildContext context, double shrinkOffset, bool overlapsContent) {
+    return Stack(
+      fit: StackFit.expand,
+      clipBehavior: Clip.none,
+      children: [
+        Container(
+          height: expandedHeight,
+          child: CustomPaint(
+            painter: _Painter(image: image),
+          ),
+        ),
+        Positioned(
+          top: 0,
+          left: 0,
+          right: 0,
+          child: SafeArea(
+              child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              IconButton(
+                icon: Icon(
+                  Icons.arrow_back_ios_sharp,
+                  color: ColorsH.white,
+                ),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              ),
+              Text(
+                'Hamburguesa especial',
+                style: TextStyle(color: ColorsH.white, fontSize: 16),
+              ),
+              Container(
+                width: 24,
+                height: 0,
+              )
+            ],
+          )),
+        ),
+        Positioned(
+          top: (expandedHeight - shrinkOffset) - 55,
+          left: MediaQuery.of(context).size.width - 100,
+          child: Opacity(
+            opacity: (1 - shrinkOffset / expandedHeight),
+            child: Container(
+              width: 48,
+              height: 48,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(100),
+                  color: ColorsH.rosa),
+              child: Icon(
+                IconsH.nofavorite,
+                color: ColorsH.white,
+              ),
+            )
+          ),
+        )
+      ],
+    );
+  }
+
+  @override
+  double get maxExtent => expandedHeight;
+
+  @override
+  double get minExtent => kToolbarHeight ;
+
+  @override
+  bool shouldRebuild(SliverPersistentHeaderDelegate oldDelegate) => true;
+}
+
+class _Painter extends CustomPainter {
+  ui.Image image;
+
+  _Painter({@required this.image});
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    // TODO: implement paint
+    final paint = Paint();
+    paint.color = ColorsH.morado;
+    paint.style = PaintingStyle.fill;
+    paint.strokeWidth = 5;
+    final path = Path();
+    path.lineTo(0, size.height + 50);
+    path.quadraticBezierTo(
+        0, size.height * 0.8 + 50, size.width * 0.3, size.height * 0.8 + 50);
+    path.lineTo(size.width * 0.8, size.height * 0.8 + 50);
+    path.quadraticBezierTo(
+        size.width, size.height * 0.8 + 50, size.width, size.height * 0.5 + 50);
+    path.lineTo(size.width, 0);
+    canvas.drawPath(path, paint);
+    canvas.drawPath(
+        path,
+        Paint()
+          ..shader = ImageShader(image, TileMode.clamp, TileMode.clamp,
+              Matrix4.identity().scaled(1.0).storage)
+          ..style = PaintingStyle.fill);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
+  }
+}
